acbecb69856571d804b137bb4d05bee3336b7ec3
KevinSheedy <kevinsheedy@gmail.com>
2018-10-03 15:27:17 +0100

    webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
-const path = require('path');
+  const path = require('path');
+  const HtmlWebpackPlugin = require('html-webpack-plugin');
+  const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-};
+  module.exports = {
+    entry: {
+      app: './src/index.js'
+      // print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './dist'
+    },
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({
+        title: 'Development'
+      })
+    ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/'
+    }
+  };
