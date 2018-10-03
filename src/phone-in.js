3228fe25ba173023ae4af4e2aa4370e67beff062
KevinSheedy <kevinsheedy@gmail.com>
2018-10-03 17:09:48 +0100

    setup event handlers

--- src/phone-in.js
+++ src/phone-in.js
@@ -1,6 +1,28 @@
 function PhoneIn(containerElem) {
 	console.log("phone-in containerElem", containerElem);
-	this.foo = "bar";
-}
+	if (!containerElem) {
+		throw new Error("PhoneIn containerElem is not a valid <div>");
+	}
 
+	this.containerElem = containerElem;
+	this.countryCodeInput = containerElem.querySelector(".phone-in__countryCode");
+	this.phoneNumberInput = containerElem.querySelector(".phone-in__phoneNumber");
+	this.suggestionsDiv = containerElem.querySelector(
+		".phone-in__countryCodeSuggestions"
+	);
+
+	const onChangeCountryCode = e => {
+		const text = e.srcElement.value;
+		console.log("this.suggestionsDiv", this.suggestionsDiv);
+
+		if (text.length < 2) {
+			this.suggestionsDiv.style.display = "none";
+		} else {
+			this.suggestionsDiv.style.display = "block";
+			this.suggestionsDiv.innerHTML = 'foo';
+		}
+	};
+
+	this.countryCodeInput.addEventListener("input", onChangeCountryCode, false);
+}
 export default PhoneIn;
